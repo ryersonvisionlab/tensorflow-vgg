9504212b5d395c8308d00ff00f775a68acefc49b
Matthew Tesfaldet <mtesfald@eecs.yorku.ca>
2019-05-24 21:38:39 -0400

    vgg19 normalized support

---
+++ vgg19_normalized.py
@@ -0,0 +1,108 @@
+import os
+import tensorflow as tf
+
+import numpy as np
+import time
+import inspect
+
+
+class Vgg19Normalized:
+    def __init__(self, vgg19_npy_path=None):
+        if vgg19_npy_path is None:
+            path = inspect.getfile(Vgg19Normalized)
+            path = os.path.abspath(os.path.join(path, os.pardir))
+            path = os.path.join(path, "vgg19_normalized_V1proto.npy")
+            vgg19_npy_path = path
+            print(vgg19_npy_path)
+
+        self.data_dict = np.load(vgg19_npy_path, encoding='latin1').item()
+        print("npy file loaded")
+
+    def build(self, bgr):
+        """
+        load variable from npy to build the VGG
+
+        :param bgr: bgr image [batch, height, width, 3]
+        """
+
+        start_time = time.time()
+        print("build model started")
+
+        self.conv1_1 = self.conv_layer(bgr, "conv1_1")
+        self.conv1_2 = self.conv_layer(self.conv1_1, "conv1_2")
+        self.pool1 = self.avg_pool(self.conv1_2, 'pool1')
+
+        self.conv2_1 = self.conv_layer(self.pool1, "conv2_1")
+        self.conv2_2 = self.conv_layer(self.conv2_1, "conv2_2")
+        self.pool2 = self.avg_pool(self.conv2_2, 'pool2')
+
+        self.conv3_1 = self.conv_layer(self.pool2, "conv3_1")
+        self.conv3_2 = self.conv_layer(self.conv3_1, "conv3_2")
+        self.conv3_3 = self.conv_layer(self.conv3_2, "conv3_3")
+        self.conv3_4 = self.conv_layer(self.conv3_3, "conv3_4")
+        self.pool3 = self.avg_pool(self.conv3_4, 'pool3')
+
+        self.conv4_1 = self.conv_layer(self.pool3, "conv4_1")
+        self.conv4_2 = self.conv_layer(self.conv4_1, "conv4_2")
+        self.conv4_3 = self.conv_layer(self.conv4_2, "conv4_3")
+        self.conv4_4 = self.conv_layer(self.conv4_3, "conv4_4")
+        self.pool4 = self.avg_pool(self.conv4_4, 'pool4')
+
+        self.conv5_1 = self.conv_layer(self.pool4, "conv5_1")
+        self.conv5_2 = self.conv_layer(self.conv5_1, "conv5_2")
+        self.conv5_3 = self.conv_layer(self.conv5_2, "conv5_3")
+        self.conv5_4 = self.conv_layer(self.conv5_3, "conv5_4")
+        self.pool5 = self.avg_pool(self.conv5_4, 'pool5')
+        
+        self.data_dict = None
+        print(("build model finished: %ds" % (time.time() - start_time)))
+
+    def avg_pool(self, bottom, name):
+        return tf.nn.avg_pool(bottom, ksize=[1, 2, 2, 1],
+                              strides=[1, 2, 2, 1], padding='SAME',
+                              name=name)
+
+    def max_pool(self, bottom, name):
+        return tf.nn.max_pool(bottom, ksize=[1, 2, 2, 1],
+                              strides=[1, 2, 2, 1], padding='SAME',
+                              name=name)
+
+    def conv_layer(self, bottom, name):
+        with tf.variable_scope(name):
+            filt = self.get_conv_filter(name)
+
+            conv = tf.nn.conv2d(bottom, filt, [1, 1, 1, 1], padding='SAME')
+
+            conv_biases = self.get_bias(name)
+            bias = tf.nn.bias_add(conv, conv_biases)
+
+            relu = tf.nn.relu(bias)
+            return relu
+
+    def fc_layer(self, bottom, name):
+        with tf.variable_scope(name):
+            shape = bottom.get_shape().as_list()
+            dim = 1
+            for d in shape[1:]:
+                dim *= d
+            x = tf.reshape(bottom, [-1, dim])
+
+            weights = self.get_fc_weight(name)
+            biases = self.get_bias(name)
+
+            # Fully connected layer. Note that the '+' operation automatically
+            # broadcasts the biases.
+            fc = tf.nn.bias_add(tf.matmul(x, weights), biases)
+
+            return fc
+
+    def get_conv_filter(self, name):
+        print('conv:', self.data_dict[name]["weights"].shape)
+        return tf.constant(self.data_dict[name]["weights"], name="filter")
+
+    def get_bias(self, name):
+        print("bias:", self.data_dict[name]["biases"].shape)
+        return tf.constant(self.data_dict[name]["biases"], name="biases")
+
+    def get_fc_weight(self, name):
+        return tf.constant(self.data_dict[name]["weights"], name="weights")
---
+++ vgg19_normalized_to_tfmodel.py
@@ -0,0 +1,17 @@
+import vgg19_normalized
+import tensorflow as tf
+
+images = tf.placeholder("float32", [None, 224, 224, 3], name="images")
+vgg = vgg19_normalized.Vgg19Normalized()
+vgg.build(images)
+
+graph = tf.get_default_graph()
+graph_def = graph.as_graph_def()
+print "graph_def byte size", graph_def.ByteSize()
+graph_def_s = graph_def.SerializeToString()
+
+save_path = "vgg19_normalized_V1proto.tfmodel"
+with open(save_path, "wb") as f:
+    f.write(graph_def_s)
+
+print "saved model to %s" % save_path
